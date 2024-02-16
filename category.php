48974ea4a9cc7abae0e86951edd8a9274a601ccd
PabloMeibi <pramirez@meibi.mx>
2024-02-16 11:04:33 -0600

    category page

--- category.php
+++ category.php
@@ -6,7 +6,28 @@
         <?php 
             $categoria = get_queried_object();
         ?>
-        <h2 class="text-primary text-center"><?php echo $categoria->name; ?></h2>
+        <h2 class="section-title text-center"><?php echo $categoria->name; ?></h2>
+        <h3 class="section-subtitle">Subcategorias:</h3>
+        <?php
+          $list_subcat = array();
+          if ( !empty($cat) ) $list_subcat = get_categories( ['hide_empty' => false, 'parent' => $cat] );
+
+          if ( count($list_subcat)):
+
+            echo "<div class='subcat'>";
+            foreach ($list_subcat as $item_subcat) {
+
+              $category_link = get_category_link( $item_subcat->term_id );
+
+              echo '<a href="'.$category_link.'">'.$item_subcat->name.'</a>';
+              echo '<div><a href="'.$category_link.'">'.$item_subcat->description.'</a></div>';
+            }
+            echo "</div>";
+
+          endif;
+        ?>
+
+        <h3 class="section-subtitle">Productos:</h3>
         <ul class="listado-grid">
             <?php
                 while( have_posts() ) { 
@@ -15,6 +36,8 @@
                 }
             ?>
         </ul>
+
+        
     </main>
 
 <?php
--- template-parts/blog.php
+++ template-parts/blog.php
@@ -1,20 +1,4 @@
-<li class="card post" data-categoria="<?php echo get_the_category()[0]->slug; ?>">
-  <a href="<?php the_permalink();?>" class="card-link"></a>
-    <?php the_category(); ?>
-    <?php the_post_thumbnail(); ?>
-    <div class="contenido">
-        <a href="<?php the_permalink(); ?>">
-            <h3><?php the_title(); ?></h3>
-        </a>
-        <p class="meta">
-            <span>By: </span>
-            <a href="<?php echo get_author_posts_url( get_the_author_meta('ID')); ?>">
-                <?php echo get_the_author_meta('display_name'); ?>
-            </a>
-        </p>
-        <p class="meta">
-            <span>Date: </span>
-            <?php the_time( get_option('date_format') ); ?>
-        </p>
-    </div>
+<li class="post" data-categoria="<?php echo get_the_category()[0]->slug; ?>">
+  <a href="<?php the_permalink();?>" class="card-link"><?php the_title(); ?></a>
+
 </li>
