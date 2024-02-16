62088ddac81a12e049f1ad33858c8204338df005
PabloMeibi <pramirez@meibi.mx>
2024-02-16 13:14:12 -0600

    about page

---
+++ page-about.php
@@ -0,0 +1,47 @@
+<?php 
+  /*
+  * Template name: About page
+  */
+  get_header(); 
+?>
+
+<?php the_content(); ?>
+
+<section class="">
+    <div class="about-parallax-container parallax" style="background-image: url('http://www.ingdeser.com/wp-content/uploads/2024/02/about-scaled.jpg');">
+        <div class="overlay">
+          <div class="content objetive-home-content">
+            <h1 style="margin-bottom: 20px;">Acerca de INGDESER</h1>
+            <p style="margin-bottom: 20px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi repudiandae inventore possimus iusto, tempore error, voluptates excepturi architecto ullam reiciendis eos nam earum quia dolorem eius facere ratione repellendus quibusdam.</p>
+          </div>
+        </div>
+      </div>
+  </section>
+
+  <section class="">
+    <div class="about-parallax-container parallax" style="background-image: url('http://www.ingdeser.com/wp-content/uploads/2024/02/vision.jpg');">
+        <div class="overlay">
+          <div class="content objetive-home-content">
+            <h1 style="margin-bottom: 20px;">Visión</h1>
+            <p style="margin-bottom: 20px;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur tenetur aliquid neque ea deleniti odit minus excepturi numquam atque quaerat in placeat dolorum, similique quod eaque accusantium nisi vitae culpa!</p>
+          </div>
+        </div>
+      </div>
+  </section>
+
+  <section class="">
+    <div class="about-parallax-container parallax" style="background-image: url('http://www.ingdeser.com/wp-content/uploads/2024/02/mision.jpg');">
+        <div class="overlay">
+          <div class="content objetive-home-content text-right">
+            <div class="text-left about-mission">
+            <h1 style="margin-bottom: 20px;">Misión</h1>
+              <p style="margin-bottom: 20px; width: 100%;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa in minima voluptatibus voluptate quae aliquid adipisci quibusdam praesentium necessitatibus vitae distinctio asperiores, quisquam cumque sed veniam perferendis, non possimus odit.</p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
+
+
+<?php get_footer(); ?>
