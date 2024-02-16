b82256691d33c25f5c459e52c94df83e5dc911cb
PabloMeibi <pramirez@meibi.mx>
2024-02-15 20:30:00 -0600

    home animation

--- front-page.php
+++ front-page.php
@@ -24,15 +24,15 @@
     <div class="first-home-icons ">
       <div class=" content">
         <div class="first-home-content">
-          <div class="first-home-txt" >
-            <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/Tranquilidad.svg" alt="Tranquilidad" >
+          <div class="first-home-txt"  data-aos="fade-up" data-aos-duration="300">
+            <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/Tranquilidad.svg" alt="Tranquilidad">
             <p><span style="font-weight:bold;color:#D25F15;">Tranquilidad: </span>Evitando que arriesgue recursos económicos, de tiempo y operación a través de un servicio de expertos calificados, que resuelven de forma rápida, eficiente y a menor costo.</p>
           </div>
-          <div class="first-home-txt">
-            <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/Comodidad.svg" alt="Comodidad" >
+          <div class="first-home-txt" data-aos="fade-up" data-aos-duration="400">
+            <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/Comodidad.svg" alt="Comodidad">
             <p><span style="font-weight:bold;color:#D25F15;">Comodidad: </span>Teniendo un proveedor que está siempre disponible, dando seguimiento continuo y acompañando a su empresa para facilitar su operación.</p>
           </div>
-          <div class="first-home-txt">
+          <div class="first-home-txt" data-aos="fade-up" data-aos-duration="500">
             <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/Atencion.svg" alt="Atención">
             <p><span style="font-weight:bold;color:#D25F15;">Atención: </span>Un trato cercano, íntegro y comprometido siempre, en todos los acuerdo convenidos.</p>
           </div>
@@ -47,31 +47,31 @@
       <p class="section-subtitle">Brindamos una amplia gama de servicios que apoyan las operciones de una gran cantidad de industrias</p>
     </div>
     <div class="grid-services content">
-      <div class="item-services">
+      <div class="item-services" data-aos="flip-left" data-aos-duration="400" >
         <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/mantenimiento-preventivo.svg" alt="Mantenimiento preventivo">
         <h2>Mantenimiento preventivo</h2>
       </div>
-      <div class="item-services">
+      <div class="item-services" data-aos="flip-left" data-aos-duration="400" >
         <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/mantenimiento-correctivo.svg" alt="Mantenimiento correctivo">
         <h2>Mantenimiento correctivo</h2>
       </div>
-      <div class="item-services">
+      <div class="item-services" data-aos="flip-left" data-aos-duration="400" >
         <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/calibracion.svg" alt="Calibracion">
         <h2>Calibración</h2>
       </div>
-      <div class="item-services">
+      <div class="item-services" data-aos="flip-left" data-aos-duration="400" >
         <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/calificacion-de-diseno.svg" alt="Calificación de diseño">
         <h2>Calificación de diseño</h2>
       </div>
-      <div class="item-services">
+      <div class="item-services" data-aos="flip-left" data-aos-duration="400" >
         <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/calificacion-de-instalacion.svg" alt="Calificación de instalación">
         <h2>Calificación de instalación</h2>
       </div>
-      <div class="item-services">
+      <div class="item-services" data-aos="flip-left" data-aos-duration="400" >
         <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/calificacion-de-operacion.svg" alt="Calificación de operación">
         <h2>Calificación de operación</h2>
       </div>
-      <div class="item-services">
+      <div class="item-services" data-aos="flip-left" data-aos-duration="400" >
         <img src="https://www.ingdeser.com/wp-content/uploads/2024/02/calificacion-de-desempeno.svg" alt="Calificación de desempeño">
         <h2>Calificación de desempeño</h2>
       </div>
