d110aa0cad44127dc48c872f157cb372af4b62b6
PabloMeibi <pramirez@meibi.mx>
2024-07-03 09:45:08 -0600

    post content category page

--- category.php
+++ category.php
@@ -11,9 +11,9 @@
         <?php
           $list_subcat = array();
           if ( !empty($cat) ) $list_subcat = get_categories( ['hide_empty' => false, 'parent' => $cat] );
-          echo '<h3 class="section-subtitle">Subcategorias:</h3>';
-
+          
           if ( count($list_subcat)):
+            echo '<h3 class="section-subtitle">Subcategorias:</h3>';
 
             echo "<div class='subcat'>";
             foreach ($list_subcat as $item_subcat) {
@@ -36,7 +36,47 @@
                 }
             ?>
         </ul>
+        <div class="post-container">
+        <?php
 
+        // Verificar si estamos en una categoría específica
+        if (is_category()) {
+            // Obtener el ID de la categoría actual
+            $category_id = get_queried_object_id();
+
+            // Configurar la consulta para obtener el último post de la categoría actual
+            $args = array(
+                'cat' => $category_id,
+                'posts_per_page' => 1, // Obtener solo un post
+            );
+
+            $latest_post_query = new WP_Query($args);
+
+            // Si hay posts en la consulta
+            if ($latest_post_query->have_posts()) {
+                while ($latest_post_query->have_posts()) {
+                    $latest_post_query->the_post(); // Configurar los datos del post actual
+
+                    // Mostrar el contenido del post
+                    ?>
+                    <div class="latest-post">
+                      <div class="post-category-content">
+                          <h2><?php the_title(); // Título del post ?></h2>
+                            <?php the_content(); // Contenido del post ?>
+                        </div>
+                    </div>
+                    <?php
+                }
+
+                // Restaurar datos originales del post
+                wp_reset_postdata();
+            } else {
+                echo '<p>No hay posts recientes en esta categoría.</p>';
+            }
+        }
+        ?>
+
+        </div>
         
     </main>
 
